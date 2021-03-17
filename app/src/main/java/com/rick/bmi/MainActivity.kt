cd43c5f3e7d431757a59c7b72b25c5cc3f059818
ricksoar <edgesoar36@gmail.com>
2021-03-18 03:38:15 +0800

    initial commit

--- app/src/main/java/com/rick/bmi/MainActivity.kt
+++ app/src/main/java/com/rick/bmi/MainActivity.kt
@@ -2,10 +2,27 @@ package com.rick.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.rick.bmi.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //  setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+    }
+
+    //AUTO FORMAT 自動排版的快捷鍵為CTRL+ALT+L
+    fun bmi(view: View) {
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_height.text.toString().toFloat()
+        val bmi = weight / (height * height)
+        Log.d("BMI",bmi.toString())
+        //lb_bmi.text=bmi.toString()
     }
 }
